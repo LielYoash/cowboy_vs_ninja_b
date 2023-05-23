f4c57274ec09a35a9b77f1fc22a3bc4dee3e179a
LielYoash <lielyoash76@gmail.com>
2023-05-23 12:32:01 +0300

    POint ready

--- sources/Point.cpp
+++ sources/Point.cpp
@@ -2,24 +2,43 @@
 using namespace ariel;
 
 Point::Point(double x, double y){
-
+    this->x = x;
+    this->y = y;
 }
 
 Point::Point(){
-
+    this->x = 0;
+    this->y = 0;
 }
 
-double Point::distance(Point other){return 0;
+double Point::distance(Point other){
+    return sqrt(pow(this->x - other.x, 2) + pow(this->y - other.y, 2));
 }
 
 void Point::print(){
+    cout << "(" << this->x << "," << this->y << ")" << endl;
 }
 
-Point Point::move_towards(Point origin, double distance, Point destination){return Point(0,0);
+Point Point::move_towards(Point origin, double distance, Point destination) {
+    if (distance < 0) {
+        throw invalid_argument("distance cannot be negative");
+    }
+    if (distance == 0) {
+        return origin;
+    }
+    if (distance == origin.distance(destination)) {
+        return destination;
+    }
+    double x = origin.x + (distance * ((destination.x - origin.x) / origin.distance(destination)));
+    double y = origin.y + (distance * ((destination.y - origin.y) / origin.distance(destination)));
+    return Point(x, y);
 }
 
-double Point::getX(){return 0;
+
+double Point::getX(){
+    return this->x;
 }
 
-double Point::getY(){return 0;
+double Point::getY(){
+    return this->y;
 }
